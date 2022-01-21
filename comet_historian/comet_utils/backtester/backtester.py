e927eb1dbd5d28e6ed01352862f9ab3366112287
chung-ejy <chung-ejy@gmail.com>
2022-01-21 14:53:10 -0500

    whut..

--- comet_historian/comet_utils/backtester/backtester.py
+++ comet_historian/comet_utils/backtester/backtester.py
@@ -33,7 +33,7 @@ class Backtester(object):
             crypto_sim["p_sign_change"] = [row[1]["velocity"] * row[1]["inflection"] < 0 for row in crypto_sim.iterrows()]
             ns.append(crypto_sim)
         final = pd.concat(ns)
-        final = final[(final["date"] < end)]
+        final = final[(final["date"] < end) * (final["value"] > 0)]
         signal = float(s/100)
         req = float(r/100)
         date = start
