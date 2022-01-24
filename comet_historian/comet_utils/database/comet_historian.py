0cc042e98bc41ec305dc937952ab82fb63e60524
chung-ejy <chung-ejy@gmail.com>
2022-01-23 20:00:41 -0500

    typo

--- comet_historian/comet_utils/database/comet_historian.py
+++ comet_historian/comet_utils/database/comet_historian.py
@@ -1,7 +1,7 @@
 from comet_utils.database.adatabase import ADatabase
 import pandas as pd
 
-class CometHIstorian(ADatabase):
+class CometHistorian(ADatabase):
     
     def __init__(self,mongouser,mongokey):
         super().__init__("comet_historian",mongouser,mongokey)
